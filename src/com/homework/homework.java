c1ac105d896e20f63b516b0d96c78aece242cafd
user <user@FB03>
2016-09-30 09:47:41 +0800

    I love oop

--- src/com/homework/homework.java
+++ src/com/homework/homework.java
@@ -1,8 +1,10 @@
 package com.homework;
 
 public class homework {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("hello oop");
+		System.out.println("hello oop");
+		System.out.println("I love oop");
 	}
 
 }
