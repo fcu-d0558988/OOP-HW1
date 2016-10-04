7087edcb6a0b085316c2a818cbe613446cb603a6
王慧 <@asus>
2016-10-04 16:50:25 +0800

    fcuhello1

---
+++ src/fcu/hello.java
@@ -0,0 +1,9 @@
+package fcu;
+
+public class hello {
+	public static void main(String[] args){
+		String lyrics="Let it go!Let it go!Cannot hold it back anymore";
+		System.out.println(lyrics);
+	}
+
+}
