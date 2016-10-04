672139d93ca15397fa7e5dcf1297efcbfd642f78
王慧 <@asus>
2016-10-04 17:05:29 +0800

    fcuhello2

--- src/fcu/hello.java
+++ src/fcu/hello.java
@@ -3,8 +3,9 @@ package fcu;
 public class hello {
 	public static void main(String[] args){
 		String lyrics="Let it go!Let it go!Cannot hold it back anymore";
-		String lyrics1=lyrics.replaceAll("it", "her");
-		System.out.println(lyrics1);
+		//String lyrics1=lyrics.replaceAll("it", "her");
+		String lyrics2=lyrics.replace("Cannot", "Can't");
+		System.out.println(lyrics2);
 	}
 
 }
